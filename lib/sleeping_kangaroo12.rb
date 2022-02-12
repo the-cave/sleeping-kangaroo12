e62e6538c7d258d8481df2721772d40e8c150a7e
Sarun Rattanasiri <midnight_w@gmx.tw>
2022-02-13 03:03:07 +0700

    new gem

---
+++ lib/sleeping_kangaroo12.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "sleeping_kangaroo12/version"
+
+module SleepingKangaroo12
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/sleeping_kangaroo12_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe SleepingKangaroo12 do
+  it "has a version number" do
+    expect(SleepingKangaroo12::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
